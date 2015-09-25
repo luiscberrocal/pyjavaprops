40861410b69b408242dc5b76aca37033a1e4b8e4
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2015-09-25 07:12:36 -0500

    Added additional tests

--- tests/test_javaproperties.py
+++ tests/test_javaproperties.py
@@ -24,11 +24,19 @@ class JavaPropertiesTestCase(unittest.TestCase):
     def test_variable_dollar(self):
         self.assertEqual('Value01/path/to/myapp', self.java_properties['Key24'])
 
+    def test_key_with_spaces_eq(self):
+        #Key17\ With\ Spaces=Value17
+        self.assertEqual('Value17', self.java_properties['Key17 With Spaces'])
+
+    def test_key_with_spaces(self):
+        #Key16\ With\ Spaces:Value16
+        self.assertEqual('Value16', self.java_properties['Key16 With Spaces'])
+
     def test_load(self):
         filename = os.path.join(TEST_DATA_FOLDER, 'simple.properties')
         simple_java_properties = JavaProperties()
         simple_java_properties.load(open(filename))
-        print(simple_java_properties)
+        self.assertEqual('1.5, 1.6.1, 1.7, 1.8,', simple_java_properties['supported.java.versions'])
 
 
 if __name__ == '__main__':
